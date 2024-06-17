2441a394fa2d3c5f41a558234f819fb6568dec87
Joonas Nätynki <joonas.natynki@gmail.com>
2024-06-17 09:16:05 +0300

    Added connection port surface interface

---
+++ Source/Mechingeer/Public/Interfaces/ConnectionPortSurface.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ConnectionPortSurface.generated.h"
+
+UINTERFACE(MinimalAPI)
+class UConnectionPortSurface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class IConnectionPortSurface
+{
+	GENERATED_BODY()
+
+public:
+};
