b663114ab47078abbc8e3ce841db742b59911548
Joonas Nätynki <joonas.natynki@gmail.com>
2024-06-17 23:13:27 +0300

    Added power connection port interface

---
+++ Source/Mechingeer/Public/Interfaces/PowerConnectionPort.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ConnectionPort.h"
+#include "UObject/Interface.h"
+#include "PowerConnectionPort.generated.h"
+
+UINTERFACE(MinimalAPI)
+class UPowerConnectionPort : public UConnectionPort
+{
+	GENERATED_BODY()
+};
+
+class IPowerConnectionPort
+{
+	GENERATED_BODY()
+
+public:
+};
